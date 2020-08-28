45e8e1c040104d4f23e1018fa21bb3500632d4a1
morpheus9631 <morpheus.hsieh@gmail.com>
2020-08-28 21:33:43 +0800

    commit

--- train_mnist_v3.py
+++ train_mnist_v3.py
@@ -9,6 +9,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 from configs.config_train import  get_cfg_defaults
+from models.model_v3 import Net
 from PIL import Image
 
 import torch
@@ -20,8 +21,6 @@ from torch.utils.data import Dataset, DataLoader
 import torchvision
 from torchvision import transforms, utils
 
-from models.model_v3 import Net
-
 
 def parse_args():
     parser = argparse.ArgumentParser(description='Ants and Bees by PyTorch')
@@ -29,10 +28,8 @@ def parse_args():
                         help="Configuration filename.")
     return parser.parse_args()
 
-
 def get_int(b):
     return int(codecs.encode(b, 'hex'), 16)
-
 
 def read_image_file(path):
     with open(path, 'rb') as f:
@@ -41,7 +38,7 @@ def read_image_file(path):
         length = get_int(data[4:8])
         num_rows = get_int(data[8:12])
         num_cols = get_int(data[12:16])
-        parsed = np.frombuffer(data, dtype=np.uint8, offset=16)
+        parsed = np.frombuffer(data, dtype=np.uint8, offset=16).copy()
         return torch.from_numpy(parsed).view(length, num_rows, num_cols)
     
 def read_label_file(path):
@@ -49,15 +46,14 @@ def read_label_file(path):
         data = f.read()
         assert get_int(data[:4]) == 2049
         length = get_int(data[4:8])
-        parsed = np.frombuffer(data, dtype=np.uint8, offset=8)
+        parsed = np.frombuffer(data, dtype=np.uint8, offset=8).copy()
         return torch.from_numpy(parsed).view(length).long()
-
 
 class myMnistDataset(Dataset):
     
     Resources = {
         'train': [ "train-images-idx3-ubyte", "train-labels-idx1-ubyte" ],
-        'valid' : [ "t10k-images-idx3-ubyte",  "t10k-labels-idx1-ubyte" ]
+        'valid': [ "t10k-images-idx3-ubyte",  "t10k-labels-idx1-ubyte"  ]
     }
     
     def __init__(self, path, train=True, transform=None, debug=True):
